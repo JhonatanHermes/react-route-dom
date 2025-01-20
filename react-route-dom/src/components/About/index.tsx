d970910089b2a5b17c86291fcabb7dce8c93164b
Jhonatan Lucco <149596203+JhonatanHermes@users.noreply.github.com>
2025-01-20 14:08:03 -0300

    Add files via upload

---
+++ react-route-dom/src/components/About/index.tsx
@@ -0,0 +1,10 @@
+import { useLocation } from 'react-router-dom'
+
+export const About = () => {
+  const { state } = useLocation();
+
+  return <div>
+  <h1>About</h1>
+  <p>{state as string}</p>
+  </div>
+}
---
+++ react-route-dom/src/components/Post/index.tsx
@@ -0,0 +1,6 @@
+export const Post = () => {
+  return <div>
+  <h1>Post</h1>
+  <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Reiciendis quia quae in nihil maxime iusto nam cumque. Ea quibusdam dolorem rerum rem temporibus doloremque earum eius provident quidem? Facilis, atque.</p>
+  </div>
+}
